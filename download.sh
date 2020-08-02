9273f40d44b2e2077893e25d4a8aebb3d64a4e75
Vlad Doster <mvdoster@gmail.com>
2020-08-02 00:17:34 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -6,6 +6,10 @@ rm -f install.sh
 
 curl -O https://raw.githubusercontent.com/vladdoster/arch-linux-installer/master/install.sh
 curl -O https://raw.githubusercontent.com/vladdoster/arch-linux-installer/master/install.conf
+curl -O https://raw.githubusercontent.com/vladdoster/dotfiles-installer/master/linux-install
+curl -O https://raw.githubusercontent.com/vladdoster/dotfiles-installer/master/shared-functions
 
+chmod +x linux-install
+chmod +x install.conf
 chmod +x install.sh
-chmod +x install.sh
+chmod +x shared-functions
