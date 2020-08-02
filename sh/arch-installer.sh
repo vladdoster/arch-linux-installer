cca72a2b71a75308542e2f9720ee81da767ed260
Vlad Doster <mvdoster@gmail.com>
2020-08-01 23:53:07 -0500

    Update arch-installer.sh

--- sh/arch-installer.sh
+++ sh/arch-installer.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-# Arch Linux Install Script (alis) installs unattended, automated
-# and customized Arch Linux system.
-# Copyright (C) 2018 picodotdev
+# Arch Linux Install Script (alis) installs unattended, automated and customized Arch Linux system.
+
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
