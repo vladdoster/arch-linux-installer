e5cd56ff5208e433bc514d6edc0c6c5693f78efe
Vlad Doster <mvdoster@gmail.com>
2020-08-02 00:12:48 -0500

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+rm -f install.conf
+rm -f install.sh
+
+curl -O https://raw.githubusercontent.com/vladdoster/arch-linux-installer/master/install.sh
+curl -O https://raw.githubusercontent.com/vladdoster/arch-linux-installer/master/install.conf
+
+chmod +x install.sh
+chmod +x install.sh
